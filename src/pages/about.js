5328e121ed653101ca684230f03c02970b4a6695
ritesh <ritesh@cloud2crowd.com>
2022-04-11 18:35:22 +0530

    Progress

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+
+const AboutPage = () => {
+    return (
+       <Layout pageTitle="About Page">
+           <div> this is about page</div>
+       </Layout>
+    )
+}
+
+export default AboutPage;
